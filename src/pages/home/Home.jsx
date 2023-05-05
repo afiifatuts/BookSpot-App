99ab04c0b9b530be7221e9357ce8c5b2f1b93390
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-05-05 09:12:19 +0700

    First commit, added header

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../../components/navbar/Navbar"
+import Header from "../../components/header/Header"
+import "./home.css"
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div><Navbar/>
+    <Header/>
+    </div>
+
+  )
+}
+
+export default Home
